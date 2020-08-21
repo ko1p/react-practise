947b41ad3d07fadc3fbf110c1fc1b36fd188b90b
ko1p <30528165+ko1p@users.noreply.github.com>
2020-08-21 09:59:51 +0300

    add previous project

--- src/App.js
+++ src/App.js
@@ -1,84 +1,14 @@
 import React, {Component} from 'react'
-import {Route, NavLink, Switch, Redirect} from 'react-router-dom'
-import './App.css'
-import About from './About/About'
-import Cars from './Cars/Cars'
-import CarDetail from './CarDetail/CarDetail'
+import Layout from './hoc/Layout/Layout'
+import Quiz from './containers/Quiz/Quiz'
 
 class App extends Component {
-
-  state = {
-    isLoggedIn: false
-  }
-
-
   render() {
     return (
-      <div>
-        <nav className="nav">
-          <ul>
-            <li>
-              <NavLink 
-              exact 
-              to="/" 
-              activeClassName={'link-active'}
-              
-              >Home</NavLink>
-            </li>
-            <li>
-              <NavLink 
-              to="/about"
-              activeStyle={{color:'blue'}}
-              >About</NavLink>
-            </li>
-            <li>
-              <NavLink 
-              to={{
-                pathname: '/cars'
-              }}
-              >Cars</NavLink>
-            </li>
-          </ul>
-        </nav>
-
-        <hr/>
-        
-        <Switch>
-          <Route 
-            path='/'
-            exact
-            render={() => <h1 style={{textAlign: 'center'}}>Home page</h1>}
-          />
-
-          { this.state.isLoggedIn ? <Route path='/about' component={About}/> : null
-          }
-
-
-          <Route 
-            path='/cars'
-            exact
-            component={Cars}
-          /> 
-
-          <Route 
-            path='/cars/:name'
-            exact
-            component={CarDetail}
-          /> 
-
-          <Redirect 
-            to='/'
-          />
-          {/* <Route 
-            render={() => <h1 style={{color: 'red', textAlign: 'center'}}>404 NOT FOUND</h1>}
-          />  */}
-        </Switch>
-          <div style={{textAlign: 'center'}}>
-            <h3>Is i logged in now? {this.state.isLoggedIn ? 'answer: TRUE' : 'answer: FALSE'}</h3>
-            <button onClick={() => this.setState({isLoggedIn: !this.state.isLoggedIn})}>TOGGLE LOGIN STATUS</button>
-          </div>
-      </div>
-    );
+      <Layout>
+        <Quiz />
+      </Layout>
+    )
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
 import {BrowserRouter} from 'react-router-dom'
-import registerServiceWorker from './registerServiceWorker';
-
+import registerServiceWorker from './registerServiceWorker'
 
 const app = (
   <BrowserRouter>
@@ -12,5 +11,5 @@ const app = (
   </BrowserRouter>
 )
 
-ReactDOM.render(app, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(app, document.getElementById('root'))
+registerServiceWorker()
