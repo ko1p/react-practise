0c8c364640e7d2990c06cc25e59a474f19cfdbf7
ko1p <30528165+ko1p@users.noreply.github.com>
2020-08-21 11:59:23 +0300

    add quizes list

--- src/containers/QuizeList/QuizeList.js
+++ src/containers/QuizeList/QuizeList.js
@@ -1,10 +1,30 @@
 import React from 'react'
+import classes from './QuizeList.module.css'
+import { NavLink } from 'react-router-dom'
 
 export default class QuizList extends React.Component {
+
+    renderQuizes = () => {
+        return(
+            [1, 2, 3].map((quiz, index) => {
+                return(
+                <li key={index}>
+                    <NavLink to={'/quiz/' + quiz}>Тест {quiz}</NavLink>
+                </li>
+                )
+            })
+        )
+    }
+
     render() {
         return(
-            <div>
-                <h1>QuizList Component</h1>
+            <div className={classes.QuizList}>
+                <div>
+                    <h1>Список текстов</h1>
+                    <ul>
+                        {this.renderQuizes()}
+                    </ul>
+                </div>
             </div>
         )
     }
