5a39f8a55d0e25c818a4f098ac52e41e2487b9d0
ko1p <30528165+ko1p@users.noreply.github.com>
2020-08-22 10:56:18 +0300

    add auth component

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,10 +1,33 @@
 import React from 'react'
+import classes from './Auth.module.css'
+import Button from '../../components/UI/Button/Button'
 
 export default class Auth extends React.Component {
+
+    loginHandler = () => {
+
+    }
+    
+    registerHandler = () => {
+    
+    }
+    
+    submitHandler = (event) => {
+        event.preventDefault()
+    }
+
     render() {
         return(
-            <div>
-                <h1>Auth Component</h1>
+            <div className={classes.Auth}>
+                <div>
+                    <h1>Авторизация</h1>
+                    <form className={classes.AuthForm} onClick={this.submitHandler}>
+                        <input/>
+                        <input/>
+                        <Button type="success" onClick={this.loginHandler}>Войти</Button>
+                        <Button type="primary" onClick={this.registerHandler}>Зарегистрироваться</Button>
+                    </form>
+                </div>
             </div>
         )
     }
