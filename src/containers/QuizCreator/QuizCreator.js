190c1f5467ed480cbc5e30204fb7d925d0048276
ko1p <30528165+ko1p@users.noreply.github.com>
2020-08-23 11:40:12 +0300

    add secelt component

--- src/containers/QuizCreator/QuizCreator.js
+++ src/containers/QuizCreator/QuizCreator.js
@@ -3,6 +3,7 @@ import classes from './QuizCreator.module.css'
 import Button from '../../components/UI/Button/Button'
 import { createControl } from '../../form/formFramework'
 import Input from '../../components/UI/Input/Input'
+import Select from '../../components/UI/Select/Select'
 
 function createOptionControl(number) {
     return createControl(
@@ -37,6 +38,7 @@ export default class QuizCreator extends React.Component {
 
     state = {
         quiz: [],
+        rightAnswerId: 1,
         formControls: createFormControls()
     } 
 
@@ -77,7 +79,26 @@ export default class QuizCreator extends React.Component {
         })
     }
 
+    selectChangeHandler = (event) => {
+        this.setState({
+            rightAnswerId: +event.target.value
+        })
+    }
+
     render() {
+
+        const select = <Select 
+            label='Выберите правильный ответ'
+            value={this.state.rightAnswerId}
+            onChange={this.selectChangeHandler}
+            options={[
+                {text: '1', value: 1},
+                {text: '2', value: 2},
+                {text: '3', value: 3},
+                {text: '4', value: 4}
+            ]}
+        />
+
         return(
             <div className={classes.QuizCreator}>
                 <div>
@@ -86,7 +107,7 @@ export default class QuizCreator extends React.Component {
 
                         { this.renderControls() }
                         
-                        <select></select>
+                        { select }
 
                         <Button
                         type='primary'
